4b8df00354185570ada558df7eff87902d291316
Priyankar <99131030+Sushitrashhhh@users.noreply.github.com>
2024-04-30 00:39:13 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,9 @@
+const kelvin = 0;
+// kelvin is kelvin lmao bitch
+const celsius = kelvin - 273;
+// idk bkc
+let fahrenheit = celsius * (9/5)+32;
+//bc idk too
+fahrenheit = Math.floor(fahrenheit)
+// ye bhi thik hai
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
